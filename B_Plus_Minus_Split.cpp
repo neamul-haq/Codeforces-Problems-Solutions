450afdeda10d52b85420bc5fc83fa3a3eeabbe1f
Neamul <neamul.cse6.bu@gmail.com>
2024-01-10 07:41:45 +0600

    Last days cf solved

---
+++ B_Plus_Minus_Split.cpp
@@ -0,0 +1,53 @@
+/*
+Problem_Link:
+AUTHOR: Neyamul_Haq
+CREATED: 09-01-2024  13:12:01
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long int ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+#define nl '\n'
+#define cyes cout << "YES" << '\n';
+#define cno cout << "NO" << '\n';
+#define print cout << ans << '\n';
+#define   Pi     2*acos(0.0)
+#define   mem(a,v)   memset(a,v,sizeof(a))
+#define sz(a) (int)(a).size()
+int const mod = 1e9+7;
+const ll inf = 1e18;
+const ll N = 1e3 ;
+#define   fast() {ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define all(x) (x).begin(), (x).end()
+void solve();
+
+int main()
+{
+#ifndef LOKAL
+    fast()
+#endif
+    ll t=1; cin >> t;
+    while(t--) solve();
+}
+
+void solve(){
+    ll i, n, m, k, j, x = 0, ans=0; cin >> n;
+    string s; cin >> s;
+    int pls=0,neg=0;
+    for(int i=0; i<n; i++){
+        if(s[i]=='+') pls++;
+        else neg++;
+    }
+    cout << abs(pls-neg) << nl;
+}
